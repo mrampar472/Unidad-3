eb52c93b5e4880ae2f39d75860124fb4b0367ac6
mrampar472 <151035847+mrampar472@users.noreply.github.com>
2024-02-22 14:06:33 +0100

    Add files via upload

---
+++ EJERCICIO 10/js/script.js
@@ -0,0 +1,13 @@
+function Calificar() {
+    	
+    let alumno = ["Stella", "Hajar", "Sofía", "Marta"];
+        
+    let notas = []
+    	notas = parseInt(prompt("¿Qué nota le pones a " + alumno[0] + "?"));
+		notas = parseInt(prompt("¿Qué nota le pones a " + alumno[1] + "?"));
+		notas = parseInt(prompt("¿Qué nota le pones a " + alumno[2] + "?"));
+		notas = parseInt(prompt("¿Qué nota le pones a " + alumno[3] + "?"));
+
+       
+}
+    
